b459468903f397e83bf52221efacdcca0483e8f7
RyanZurrin <ryanzurrin@ryanzurrin.com>
2022-09-07 23:37:12 -0400

    generalized

--- locker_problem.py
+++ locker_problem.py
@@ -9,7 +9,7 @@
 # Here’s the question: “Which lockers are left open after all 100 students have walked the row of lockers?”
 
 
-def main():
+def locker_problem():
     # get as input from user the number of lockers and students
     # create a list of lockers with the number of lockers
     # create a list of students with the number of students
@@ -26,16 +26,28 @@ def main():
             # If the locker number is a multiple of the student number
             if locker % student == 0:
                 # Change the state of the locker
-                print("student number: ", student, "locker number: ", locker)
                 lockers[locker] = not lockers[locker]
     # Loop through each locker
     for locker in range(1, total_lockers):
         # If the locker is open
         if lockers[locker]:
             # Print the locker number
-            print(locker + 1, end=' ')
+            print(locker, end=' ')
     print()
+    return lockers
 
 
-if __name__ == '__main__':
-    main()
+def get_factors(lockers):
+    factors = {}
+    for i in range(1, lockers + 1):
+        factors[i] = []
+        for j in range(1, i + 1):
+            if i % j == 0:
+                factors[i].append(j)
+    return factors
+
+lockers = locker_problem()
+factors = get_factors(len(lockers))
+for i in range(1, len(lockers)):
+    if lockers[i]:
+        print(f"Locker {i} is open. Factors: {factors[i]}")
