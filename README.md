32cc17c5e931c02ba6b43dbeb21358966ca1a840
RyanZurrin <ryanzurrin@ryanzurrin.com>
2022-12-08 11:43:17 -0500

    updated readme with problem statement, removing it from script file for better readability.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # LockerProblem
 
-Simple program to solve the locker problem
+N lockers numbered 1 to N with N students lined up in front of lockers 
+starting at locker 1. The first student opens all the lockers. The second
+student closes every second locker. The third student toggles every third
+locker. The fourth student toggles every fourth locker. That same pattern
+continues for all N students and N lockers.
+
+## Here’s the problem to be solved:  
+What is the state of the lockers after all N students have completed their
+pass through the lockers?
--- locker_problem.py
+++ locker_problem.py
@@ -1,13 +1,3 @@
-# 100 lockers numbered 1 to 100 with 100 students lined up in front of those
-# 100 lockers. The 1st student opens every locker. The 2nd student closes
-# every 2nd locker. The 3rd student changes every 3rd locker; if it’s closed,
-# she opens it; if it’s open, she closes it. The 4th student changes every
-# fourth locker. The 5th student changes every 5th locker. That same pattern
-# continues for all 100 students
-
-# Here’s the question: “Which lockers are left open after all 100 students
-# have walked the row of lockers?”
-
 import time
 
 __author__ = "Ryan Zurrin"
